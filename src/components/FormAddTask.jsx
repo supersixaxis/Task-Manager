44605906e578716d929a61298e0743f5f083246e
supersixaxis <appert.nathan@outlook.fr>
2023-12-21 13:23:57 +0100

    faire les popin sur les formulaires de tableau

--- src/components/FormAddTask.jsx
+++ src/components/FormAddTask.jsx
@@ -3,33 +3,45 @@ import React, { useState } from 'react';
 export default function FormAddTask({ tables, addTaskTable }) {
     const [idTableSelected, setIdTableSelected] = useState(0)
     const [newTableTask, setNewTableTask] = useState('');
+    const [isPopinVisible, setPopinVisible] = useState(false);
+
+    const handleButtonClick = () => {
+      setPopinVisible(!isPopinVisible);
+    };
   return (
-    <form
-    onSubmit={(e) => {
-      e.preventDefault();
-      if (idTableSelected.toString() === '0') {
-        alert('Veuillez sélectionner un tableau à éditer');
-      } else if (newTableTask.trim() === "") {
-        alert('Veuillez sélectionner un tableau à éditer');
-      }
-       else {
-        addTaskTable(idTableSelected, newTableTask);
-        setIdTableSelected('0');
-        setNewTableTask('');
-      }
-    }}
-  >
-        <label>Ajouter une tâche au tableau</label>
-      <select  value={idTableSelected} onChange={(e) => setIdTableSelected(e.target.value)}>
-      <option value={0}>---</option>
-        {tables.map((table) => (
-          <option key={table.id} value={table.id}>
-            {table.title}
-          </option>
-        ))}
-      </select>
-      <input type="text" value={newTableTask}    placeholder="Nom de la tache" onChange={(e) => setNewTableTask(e.target.value)}/>
-      <button type='submit'>Ajouter une tâche</button>
-      </form>
+    <div>
+      <button onClick={handleButtonClick}>Ajouter une tâche</button>
+      {isPopinVisible && (
+      <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (idTableSelected.toString() === '0') {
+          alert('Veuillez sélectionner un tableau à éditer');
+        } else if (newTableTask.trim() === "") {
+          alert('Veuillez sélectionner un tableau à éditer');
+        }
+        else {
+          addTaskTable(idTableSelected, newTableTask);
+          setIdTableSelected('0');
+          setNewTableTask('');
+          setPopinVisible(false);
+        }
+      }}
+    >
+          <label>Ajouter une tâche au tableau</label>
+        <select  value={idTableSelected} onChange={(e) => setIdTableSelected(e.target.value)}>
+        <option value={0}>---</option>
+          {tables.map((table) => (
+            <option key={table.id} value={table.id}>
+              {table.title}
+            </option>
+          ))}
+        </select>
+        <input type="text" value={newTableTask}    placeholder="Nom de la tache" onChange={(e) => setNewTableTask(e.target.value)}/>
+        <button type='submit'>Ajouter une tâche</button>
+        
+        </form>
+      )}
+      </div>
   )
 }
--- src/components/SelectTableDelete.jsx
+++ src/components/SelectTableDelete.jsx
@@ -4,27 +4,36 @@ import React, { useState } from 'react';
 
 const SelectTableDelete = ({ tables, deleteTable }) => {
   const [idTableSelected, setIdTableSelected] = useState(0)
+  const [isPopinVisible, setPopinVisible] = useState(false);
+  const handleButtonClick = () => {
+    setPopinVisible(!isPopinVisible);
+  };
   return (
-    
-      <form onSubmit={(e) => {
-        e.preventDefault()
-        if(idTableSelected.toString() === '0') {
-           alert('Veuiller sélectionner un tableau à supprimer')
-        }
-        deleteTable(idTableSelected)
-        setIdTableSelected('0')
-      }}>
-        <label>Supprimer un tableau</label>
-      <select  value={idTableSelected} onChange={(e) => setIdTableSelected(e.target.value)}>
-      <option value={0}>---</option>
-        {tables.map((table) => (
-          <option key={table.id} value={table.id}>
-            {table.title}
-          </option>
-        ))}
-      </select>
-      <button onSubmit={deleteTable}>Delete</button>
-      </form>
+      <div>
+          <button onClick={handleButtonClick}>Supprimer un tableau</button>
+          {isPopinVisible && (
+          <form onSubmit={(e) => {
+            e.preventDefault()
+            if(idTableSelected.toString() === '0') {
+              alert('Veuiller sélectionner un tableau à supprimer')
+            }
+            deleteTable(idTableSelected)
+            setIdTableSelected('0')
+            setPopinVisible(false);
+          }}>
+            <label>Supprimer un tableau</label>
+          <select  value={idTableSelected} onChange={(e) => setIdTableSelected(e.target.value)}>
+          <option value={0}>---</option>
+            {tables.map((table) => (
+              <option key={table.id} value={table.id}>
+                {table.title}
+              </option>
+            ))}
+          </select>
+          <button onSubmit={deleteTable}>Delete</button>
+        </form>
+          )}
+      </div>
     
       
   );
