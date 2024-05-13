bbd2fe9d18752b23a9bcc63ef3f4567416efc379
supersixaxis <86681614+supersixaxis@users.noreply.github.com>
2024-05-13 17:02:33 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
 import {BrowserRouter} from 'react-router-dom'
-import { store } from './redux/Store.js'
+import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
 
 
